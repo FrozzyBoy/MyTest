3bb397485fd3039655dc77916c326adde345c268
Artsiom Marozau <artsiom_marozau@epam.com>
2016-01-11 10:41:11 +0300

    first commit

---
+++ MyTest.Test/MagicTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyTest.Test
+{
+	public class MagicTest
+	{
+		[Fact]
+		public void OnTestCastSpellShouldBeAccio()
+		{
+			var magic = new Magic();
+			string castedSpell = magic.CastSpell();
+			Assert.Equal<string>("Accio", castedSpell);
+		}
+	}
+}
---
+++ MyTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var magic = new Magic();
+			Console.WriteLine(magic.CastSpell());
+		}
+	}
+}
